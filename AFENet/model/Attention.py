9a09ac7ec734f6bc0e74612183d57eb20913aeef
oucfm <102983379+oucfm@users.noreply.github.com>
2025-02-27 18:32:26 +0800

    Add files via upload

---
+++ AFENet/model/Attention.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from einops import rearrange
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, dim, num_head, bias):
+        super(CrossAttention, self).__init__()
+        self.num_head = num_head
+        self.temperature = nn.Parameter(torch.ones(num_head, 1, 1), requires_grad=True)
+
+        self.q = nn.Conv2d(dim, dim, kernel_size=1, bias=bias)
+        self.q_dwconv = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1, groups=dim, bias=bias)
+
+        self.kv = nn.Conv2d(dim, dim * 2, kernel_size=1, bias=bias)
+        self.kv_dwconv = nn.Conv2d(dim * 2, dim * 2, kernel_size=3, stride=1, padding=1, groups=dim * 2, bias=bias)
+
+        self.project_out = nn.Conv2d(dim, dim, kernel_size=1, bias=bias)
+
+    def forward(self, x, y):
+        # x -> q, y -> kv
+        assert x.shape == y.shape, 'The shape of feature maps from image and features are not equal!'
+
+        b, c, h, w = x.shape
+
+        q = self.q_dwconv(self.q(x))
+        kv = self.kv_dwconv(self.kv(y))
+        k, v = kv.chunk(2, dim=1)
+
+        q = rearrange(q, 'b (head c) h w -> b head c (h w)', head=self.num_head)
+        k = rearrange(k, 'b (head c) h w -> b head c (h w)', head=self.num_head)
+        v = rearrange(v, 'b (head c) h w -> b head c (h w)', head=self.num_head)
+
+        q = torch.nn.functional.normalize(q, dim=-1)
+        k = torch.nn.functional.normalize(k, dim=-1)
+
+        attn = q @ k.transpose(-2, -1) * self.temperature
+        attn = attn.softmax(dim=-1)
+
+        out = attn @ v
+
+        out = rearrange(out, 'b head c (h w) -> b (head c) h w', head=self.num_head, h=h, w=w)
+
+        out = self.project_out(out)
+        return out
+
